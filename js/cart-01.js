831b264d370ff5aae273027e9e4d27a1b7bb5609
ExLuZiVe53 <imstudent27@gmail.com>
2023-07-18 18:30:18 +0200

    Update cart-01.js

--- js/cart-01.js
+++ js/cart-01.js
@@ -1,1 +1,15 @@
 // Відстежуємо клік по сторінкі
+window.addEventListener('click', function (event) {
+  // Перевіряємо чи клік був проведений на кнопку "Добавити в корзину"
+  if (event.target.hasAttribute('data-cart')) {
+    // Знаходимо карточку з товаром в середині ми клікнули
+    const card = event.target.closest('.card');
+    // Збираємо дані з цього товара та записуємо один в об'єкт productInfo
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector('.product-img').getAttribute('src'),
+      title: card.querySelector('.item-title').innerText,
+      itemInBox: card.querySelector('[data-items-in-box]').innerText,
+    };
+  }
+});
